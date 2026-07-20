bcfffc6822e279525fe1263696b753fcfe7411cf
sophia <sophiancmariano@gmail>
2026-07-20 10:12:12 -0300

    correção de alguns erros

--- src/main/java/com/example/demo/sat/config/SecurityConfig.java
+++ src/main/java/com/example/demo/sat/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
 
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
+                		"/",
                     "/login",
                     "/login-error",
                     "/css/**",
